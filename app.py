4a243a25e3d137602c7eff1ca6c00d7dc96878d1
Sanzinan Afrose <76949859+sanzinanafrose@users.noreply.github.com>
2026-04-17 01:31:49 +0600

    Deployed
    Change app.run to use environment variable for port and disable debug mode.

--- app.py
+++ app.py
@@ -1090,4 +1090,5 @@ def admin_delete_user(user_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
